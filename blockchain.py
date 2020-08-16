0581e3150a96135c6ebea3da5a372b4e8601b939
shalev <shalev130202@gmail.com>
2020-08-16 14:53:35 +0300

    add mine function and block chain valid function

--- blockchain.py
+++ blockchain.py
@@ -24,28 +24,47 @@ class Block:
                                                                          self.previeos_hash))
 
 class BlockChain():
-    difficulty=4
+    difficulty=5
     def __init__(self,chain=[]):
         self.chain=chain
     def add(self,block):
-        self.chain.append({
-            'hash':block.hash(),
-            'previous':block.previeos_hash,
-            'number':block.number,
-            'data':block.data,
-            'nonce':block.nonce
-        })
+        self.chain.append(block)
 
     def __str__(self):
         ret=""
         for block in self.chain:
-            ret+=str(block)
+            ret+=str(block)+"\n"
         return ret
+    def mine(self,block):
+        try:
+            block.previeos_hash=self.chain[-1].hash()
+        except IndexError:
+            pass
+        while True:
+            if block.hash()[:self.difficulty]=="0"*self.difficulty:
+                self.add(block);break
+            else:
+                block.nonce+=1
+    def isValid(self):
+        for i in range(1,len(self.chain)):
+            if i==1:
+                if self.chain[0].hash()[:self.difficulty] != "0" * self.difficulty:
+                    return False
+            if self.chain[i].hash()[:self.difficulty]!="0"*self.difficulty:
+                return False
 
+            if self.chain[i-1].hash()!=self.chain[i].previeos_hash:
+                return False
+            return  True
 def main():
-    block=Block("welcome",1)
+
     bc=BlockChain()
-    bc.add(block)
-    print(bc)
+    database=["hello world","whats up","hello","bye"]
+    num=0
+    for data in database:
+        num+=1
+        bc.mine(Block(data,num))
+    print(bc.isValid())
+
 if __name__ == '__main__':
     main()
